856fbae0b125e7c0f91769a88de6fdc0721b5e6b
Gastón Ríos <g.rios@outlook.com>
2023-08-09 15:25:46 -0300

    feat/no-ref/added-base-files

---
+++ STAGGI-Budget-API/Data/BudgetContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using STAGGI_Budget_API.Models;
+
+namespace STAGGI_Budget_API.Data
+{
+    public class BudgetContext  : DbContext
+    {
+        public BudgetContext(DbContextOptions<BudgetContext> options) : base(options) { }
+        public DbSet<BudUser> BudUsers { get; set; }
+    }
+}
--- STAGGI-Budget-API/Program.cs
+++ STAGGI-Budget-API/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using STAGGI_Budget_API.Data;
+using STAGGI_Budget_API.Models;
+using STAGGI_Budget_API.Repositories;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BudgetContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+
+builder.Services.AddScoped<IBudUserRepository, BudUserRepository>();
+
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("CorsPolicy",
+//        policy =>
+//        {
+//            policy
+//                .WithOrigins("http://localhost:4200, localhost:4200")
+//                .AllowAnyMethod()
+//                .AllowCredentials()
+//                .SetIsOriginAllowed((host) => true)
+//                .AllowAnyHeader();
+//        });
+//});
 
 var app = builder.Build();
 
@@ -16,8 +45,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Enable CORS
+app.UseCors(builder =>
+    builder.WithOrigins("http://localhost:3000") // Cambia esto con el origen correcto
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials()
+); // Habilita el envío de credenciales}
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
