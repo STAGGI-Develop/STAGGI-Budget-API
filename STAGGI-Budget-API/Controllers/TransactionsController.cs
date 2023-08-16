722f8dc2c613054c808d98a23a1e95006587499e
ignacio.dibella.n@gmail.com <ignacio.dibella.n@gmail.com>
2023-08-16 09:34:03 -0300

    Corrección de errores.

--- STAGGI-Budget-API/Controllers/TransactionsController.cs
+++ STAGGI-Budget-API/Controllers/TransactionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using STAGGI_Budget_API.DTOs;
 using STAGGI_Budget_API.Services;
 
 namespace STAGGI_Budget_API.Controllers
@@ -8,24 +9,23 @@ namespace STAGGI_Budget_API.Controllers
     [ApiController]
     public class TransactionsController : ControllerBase
     {
-        private readonly IAccountService _accountService;
-        public TransactionsController(IAccountService accountService)
+        private readonly ITransactionService _transactionService;
+        public TransactionsController(ITransactionService transactionService)
         {
-            _accountService = accountService;
+            _transactionService = transactionService;
         }
 
-        [HttpPost]
-        public IActionResult ModifyTransaction()
+        [HttpPut]
+        public IActionResult ModifyTransaction(long transactionId, [FromBody] TransactionDTO transactionDTO)
         {
-            //var result = _accountService.GetAll();
-            var result = true;
+            var result = _transactionService.ModifyTransaction(transactionId, transactionDTO);
 
-            //if (!result.IsSuccess)
-            //{
-            //    return StatusCode(result.Error.Status, result.Error);
-            //}
+            if (!result.IsSuccess)
+            {
+                return StatusCode(result.Error.Status, result.Error);
+            }
 
-            return StatusCode(201, result);//.Ok);
+            return StatusCode(201, result.Ok);
         }
     }
 }
