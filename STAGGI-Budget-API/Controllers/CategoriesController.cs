2dc61cbfb452d5c412fcd3223926308dc42b5157
Sebb <sebastianfabretti@gmail.com>
2023-08-17 10:26:32 -0300

    Add categories controller

---
+++ STAGGI-Budget-API/Controllers/CategoriesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using STAGGI_Budget_API.Services;
+using System.Text.RegularExpressions;
+
+namespace STAGGI_Budget_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var categories = _categoryService.GetAll();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult PostCategory(string category)
+        {
+            Regex regexName = new Regex("[a-zA-Z ]");
+            Match categoryMatch = regexName.Match(category);
+
+            if (category.Length > 15)
+            {
+                return Forbid("La longitud de la categoria supera el maximo");
+            }
+            
+            if (!categoryMatch.Success)
+            {
+                return Forbid("La categoria solo acepta letras");
+            }           
+            return Created("", category);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory()
+        {
+            return Ok();
+        }
+    }
+}
