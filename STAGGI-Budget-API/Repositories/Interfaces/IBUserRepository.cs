98fcc0747409b9f533b6d6a574412a9dfb61a81b
Gastón Ríos <g.rios@outlook.com>
2023-08-18 09:53:12 -0300

    fix conflict

---
+++ STAGGI-Budget-API/Repositories/Interfaces/IBUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using STAGGI_Budget_API.Models;
+
+namespace STAGGI_Budget_API.Repositories.Interfaces
+{
+    public interface IBUserRepository
+    {
+        IEnumerable<BUser> GetAll();
+        void Save(BUser budUser);
+        BUser FindById(string id);
+        BUser FindByEmail(string email);
+    }
+}
