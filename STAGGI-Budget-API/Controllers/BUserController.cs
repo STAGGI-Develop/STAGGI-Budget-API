a696c4b8b8a6822fa40921cf306c65f69f829327
ignacio.dibella.n@gmail.com <ignacio.dibella.n@gmail.com>
2023-08-28 19:13:58 -0300

    agregado de endpoint para registrar clientes y suscripciones

--- STAGGI-Budget-API/Controllers/BUserController.cs
+++ STAGGI-Budget-API/Controllers/BUserController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using STAGGI_Budget_API.DTOs;
+using STAGGI_Budget_API.Models;
+using STAGGI_Budget_API.Services;
 using STAGGI_Budget_API.Services.Interfaces;
 
 namespace STAGGI_Budget_API.Controllers
@@ -9,9 +14,13 @@ namespace STAGGI_Budget_API.Controllers
     public class BUserController : ControllerBase
     {
         private readonly IBUserService _buserService;
-        public BUserController(IBUserService buserService)
+        private readonly IAuthService _authService;
+        private readonly UserManager<BUser> _userManager;
+        public BUserController(IBUserService buserService, UserManager<BUser> userManager, IAuthService authService)
         {
             _buserService = buserService;
+            _userManager = userManager;
+            _authService = authService;
         }
 
         [HttpGet]
@@ -26,9 +35,53 @@ namespace STAGGI_Budget_API.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post()
+        [AllowAnonymous]
+        public IActionResult Register([FromBody] RegisterRequestDTO registerRequestDTO)
         {
-            return Ok();
+            var result = _buserService.RegisterBUser(registerRequestDTO, _userManager);
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(result.Error.Status, result.Error);
+            }
+            return Ok(result.Ok);
+        }
+
+        [HttpGet ("profile")]
+        [Authorize]
+        public IActionResult GetProfile()
+        {
+            var authorizationHeader = HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+            var token = authorizationHeader?.Substring(7);
+            var userEmail = _authService.GetEmailFromToken(token);
+
+            var result = _buserService.GetUserProfile(userEmail);
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(result.Error.Status, result.Error);
+            }
+            return Ok(result.Ok);
+
+        }
+
+
+        [HttpPost("subscribe")]
+        public IActionResult Subscribe()
+        {
+            var authorizationHeader = HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+            var token = authorizationHeader?.Substring(7);
+            var userEmail = _authService.GetEmailFromToken(token);
+
+            var result = _buserService.Subscribe(userEmail); 
+
+
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(result.Error.Status, result.Error);
+            }
+            return Ok(result.Ok);
         }
     }
 }
---
+++ STAGGI-Budget-API/DTOs/UserProfileDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace STAGGI_Budget_API.DTOs
+{
+    public class UserProfileDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; } //Así cuando lo modifiquen se cambie el email. sino vamos a andar dando vueltas
+        public string? PhoneNumber { get; set; }
+        public string? SuscriptionState { get; set; } //0: Classic || 1:Premium
+        public DateTime? EndDate { get; set; }
+    }
+}
